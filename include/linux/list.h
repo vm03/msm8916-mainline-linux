96fb4692cdaebe32cb80993c6d32cf421d29cf24
Ingo Molnar <mingo@kernel.org>
2021-06-06 16:03:57 +0200

    headers/prep: list.h: Introduce the list_for_each_reverse() method
    The list_head counterpart of list_for_each_entry_reverse() was missing,
    add it.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/list.h
+++ include/linux/list.h
@@ -573,6 +573,14 @@ static inline void list_splice_tail_init(struct list_head *list,
 	for (pos = (head)->next; pos != (head); pos = pos->next)
 
 /**
+ * list_for_each	-	iterate over a list, backwards
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @head:	the head for your list.
+ */
+#define list_for_each_reverse(pos, head) \
+	for (pos = (head)->prev; pos != (head); pos = pos->prev)
+
+/**
  * list_for_each_continue - continue iteration over a list
  * @pos:	the &struct list_head to use as a loop cursor.
  * @head:	the head for your list.
