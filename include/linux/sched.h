48bfed306336031385b9ac46287a1f53632fa504
Ingo Molnar <mingo@kernel.org>
2021-06-07 12:39:04 +0200

    headers/uninline: Uninline task_index_to_char()
    While it's a simple function, it's not critical, plus it brings in an unnecessary
    <linux/ilog2.h> dependency into <linux/sched.h>.
    
    Move it next to show_state().
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1610,14 +1610,7 @@ static inline unsigned int task_state_index(struct task_struct *tsk)
 	return fls(state);
 }
 
-static inline char task_index_to_char(unsigned int state)
-{
-	static const char state_char[] = "RSDTtXZPI";
-
-	BUILD_BUG_ON(1 + ilog2(TASK_REPORT_MAX) != sizeof(state_char) - 1);
-
-	return state_char[state];
-}
+extern char task_index_to_char(unsigned int state);
 
 static inline char task_state_to_char(struct task_struct *tsk)
 {
