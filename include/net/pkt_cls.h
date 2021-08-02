a96c614a2b33401ffff2372e3477a58c4a4bfdb4
Ingo Molnar <mingo@kernel.org>
2021-08-02 22:45:51 +0200

    headers/deps: net: Optimize <net/pkt_cls.h> dependencies
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
      before: | #include <net/pkt_cls.h>                | LOC: 27,739 | headers:  449
       after: | #include <net/pkt_cls.h>                | LOC: 25,924 | headers:  431
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/pkt_cls.h
+++ include/net/pkt_cls.h
@@ -5,11 +5,14 @@
 #include <linux/skbuff_api.h>
 #include <linux/pkt_cls.h>
 #include <linux/workqueue.h>
+#include <linux/slab.h>
 
 #include <net/rtnetlink_api.h>
 #include <net/sch_generic.h>
-#include <net/act_api.h>
 #include <net/net_namespace_api.h>
+
+struct tcf_proto;
+struct tcf_proto_ops;
 
 /* TC action not accessible from user space */
 #define TC_ACT_CONSUMED		(TC_ACT_VALUE_MAX + 1)
