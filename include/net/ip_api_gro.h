96d77820e8627998c5ddb55753936514c44a3761
Ingo Molnar <mingo@kernel.org>
2021-08-08 13:14:51 +0200

    headers/deps: net/gso: Move inet_gro_compute_pseudo() from <net/ip.h> to its own header in <net/ip_api_gro.h>
    This helps decouple <net/ip.h> from <linux/netdevice_api.h>.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- include/net/ip_api_gro.h
+++ include/net/ip_api_gro.h
@@ -1,1 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _IP_API_GRO_H
+#define _IP_API_GRO_H
+
 #include <net/ip.h>
+
+#include <linux/netdevice_api.h>
+
+static inline __wsum inet_gro_compute_pseudo(struct sk_buff *skb, int proto)
+{
+	const struct iphdr *iph = skb_gro_network_header(skb);
+
+	return csum_tcpudp_nofold(iph->saddr, iph->daddr,
+				  skb_gro_len(skb), proto, 0);
+}
+
+#endif	/* _IP_API_GRO_H */
