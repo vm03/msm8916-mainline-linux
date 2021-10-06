6d44ab38c84d258afc29977b1dca0644c64fe8ba
Ingo Molnar <mingo@kernel.org>
2021-10-06 11:39:57 +0200

    headers/deps: kernel: Split <linux/restart_block.h> into <linux/restart_block_types.h> and <linux/restart_block_api.h>
    <linux/restart_block.h> is included in <linux/sched.h>, and it indirectly
    requires the inclusion of <linux/thread_info.h> definitions - which we'd like to
    decouple of <linux/sched.h>.
    
              --------------------------------------------------------------------
              | Combined, preprocessed C code size of header, without line markers,
              | with comments stripped:
              -------------------------
              | #include <linux/restart_block.h>        | .... | .... |
    
    In order to reduce this impact, we are splitting it into three parts:
    
     - <linux/restart_block_types.h>: lightweight header with only type definitions
                                      and initializers.
    
     - <linux/restart_block_api.h>:   API declarations and inline functions. This header
                                      is inevitably more heavyweight.
    
     - <linux/restart_block.h>: Simple default header that right now includes both
                                the types and the API headers, but will only
                                use the types header once all affected code
                                is converted.
    
    No change in functionality in this first step.
    
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

---
+++ include/linux/restart_block_api.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Common syscall restarting data
+ */
+#ifndef __LINUX_RESTART_BLOCK_API_H
+#define __LINUX_RESTART_BLOCK_API_H
+
+#include <linux/restart_block_types.h>
+
+#include <linux/compiler.h>
+#include <linux/types.h>
+#include <linux/time64.h>
+#include <linux/errno.h>
+
+#include <linux/thread_info.h>
+
+extern long do_no_restart_syscall(struct restart_block *parm);
+
+#ifndef arch_set_restart_data
+#define arch_set_restart_data(restart) do { } while (0)
+#endif
+
+static inline long set_restart_fn(struct restart_block *restart,
+					long (*fn)(struct restart_block *))
+{
+	restart->fn = fn;
+	arch_set_restart_data(restart);
+	return -ERESTART_RESTARTBLOCK;
+}
+
+#endif /* __LINUX_RESTART_BLOCK_API_H */
