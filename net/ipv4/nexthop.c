5fd5208553cb53f99b8d607ca8a6b8121a34276a
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:48:40 +0100

    headers/deps: Add header dependencies to .c files: <net/ip_fib_api.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- net/ipv4/nexthop.c
+++ net/ipv4/nexthop.c
@@ -5,6 +5,7 @@
  * Copyright (c) 2017-19 David Ahern <dsa@cumulusnetworks.com>
  */
 
+#include <net/ip_fib_api.h>
 #include <linux/workqueue_api.h>
 #include <linux/vmalloc.h>
 #include <linux/sched.h>
