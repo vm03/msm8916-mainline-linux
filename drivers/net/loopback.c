c937579c3dd1beb7612f94278e0a72ea9dd81158
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:48:53 +0100

    headers/deps: Add header dependencies to .c files: <net/sch_generic.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/net/loopback.c
+++ drivers/net/loopback.c
@@ -24,6 +24,7 @@
  *		Alexey Kuznetsov:	Potential hang under some extreme
  *					cases removed.
  */
+#include <net/sch_generic.h>
 #include <net/sch_generic_api.h>
 #include <net/route_api.h>
 #include <net/dst_api.h>
--- drivers/net/vrf.c
+++ drivers/net/vrf.c
@@ -9,6 +9,7 @@
  * Based on dummy, team and ipvlan drivers
  */
 
+#include <net/sch_generic.h>
 #include <net/rtnetlink_api.h>
 #include <net/route_api.h>
 #include <net/net_namespace_api.h>
