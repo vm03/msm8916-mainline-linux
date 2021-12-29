1dc21e73d82257e63a051a6e87fd839fec53a9d4
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:42:59 +0100

    headers/deps: Add header dependencies to .c files: <linux/kprobes.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -12,6 +12,7 @@
  * management can be a bitch. See 'mm/memory.c': 'copy_page_range()'
  */
 
+#include <linux/kprobes.h>
 #include <linux/ioprio.h>
 #include <linux/hrtimer_api.h>
 #include <linux/highmem.h>
