f786cc160b738840b8920792fa1fda529da3c617
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:45:10 +0100

    headers/deps: Add header dependencies to .c files: <linux/percpu-refcount-api.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -9,6 +9,7 @@
  *                        of people at Linux Expo.
  */
 
+#include <linux/percpu-refcount-api.h>
 #include <linux/mutex_api.h>
 #include <linux/mm_api.h>
 #include <linux/kobject_api.h>
