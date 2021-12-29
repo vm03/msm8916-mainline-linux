80900c7d3a92f1fe7f6f829db673fd545da76396
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:48:33 +0100

    headers/deps: Add header dependencies to .c files: <linux/writeback_api.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- fs/9p/vfs_addr.c
+++ fs/9p/vfs_addr.c
@@ -6,6 +6,7 @@
  *  Copyright (C) 2002 by Ron Minnich <rminnich@lanl.gov>
  */
 
+#include <linux/writeback_api.h>
 #include <linux/uio_api.h>
 #include <linux/refcount_api.h>
 #include <linux/mm_api.h>
