a1e46029ec2fe6ac60bce35eee055a1b541b176d
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:47:36 +0100

    headers/deps: Add header dependencies to .c files: <linux/sysctl.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- fs/fscache/main.c
+++ fs/fscache/main.c
@@ -6,6 +6,7 @@
  */
 
 #define FSCACHE_DEBUG_LEVEL CACHE
+#include <linux/sysctl.h>
 #include <linux/kobject_api.h>
 #include <linux/hash.h>
 #include <linux/cpumask_api.h>
--- net/rds/tcp.c
+++ net/rds/tcp.c
@@ -30,6 +30,7 @@
  * SOFTWARE.
  *
  */
+#include <linux/sysctl.h>
 #include <linux/spinlock_api.h>
 #include <linux/netdevice_api.h>
 #include <linux/mutex_api.h>
