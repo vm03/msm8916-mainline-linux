c2200a1026b8a90410364a903e506cd226ee70b1
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:45:37 +0100

    headers/deps: Add header dependencies to .c files: <linux/rcuwait_api.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7,6 +7,7 @@
  *  Copyright (C) 1991-2002  Linus Torvalds
  */
 #define CREATE_TRACE_POINTS
+#include <linux/rcuwait_api.h>
 #include <linux/rculist.h>
 #include <linux/prefetch.h>
 #include <linux/pgtable_api.h>
