87bb216d0f14fb20c4f8b89f208e220dabff6464
Ingo Molnar <mingo@kernel.org>
2021-12-29 14:48:44 +0100

    headers/deps: Add header dependencies to .c files: <net/ndisc_api.h>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/staging/gdm724x/gdm_lte.c
+++ drivers/staging/gdm724x/gdm_lte.c
@@ -3,6 +3,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <net/ndisc_api.h>
 #include <linux/skbuff_api_extra.h>
 #include <linux/netdevice_api.h>
 #include <linux/etherdevice_api.h>
